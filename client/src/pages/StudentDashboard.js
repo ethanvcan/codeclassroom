f9909a5c6862f2dc32b1fba9ed20552b15d204f8
ethanvcan <ethanvcan@gmail.com>
2025-06-13 23:35:22 -0700

    Updated classroom join error

--- client/src/pages/StudentDashboard.js
+++ client/src/pages/StudentDashboard.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './StudentDashboard.css';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const StudentDashboard = () => {
   const username = localStorage.getItem('username');
